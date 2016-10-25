c99e2ddcb8fe9e59547eead19f4d7e56859ed3bf
Kyle Baum <username@users.noreply.github.com>
2016-10-24 21:56:44 -0500

    homework

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,70 @@
 class GameController < ApplicationController
-def user_plays_rock
+  def user_plays_rock
+    @user_move = "rock"
+    @computer_move = ["rock", "paper", "scissors"].sample
 
+    if @user_move == @computer_move
+      @game_result = "tied"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @game_result = "won"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @game_result = "lost"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @game_result = "lost"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @game_result = "won"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @game_result = "lost"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @game_result = "won"
+    end
 
-    moves = ["rock", "paper", "scissors"]
-     @computer_move = moves.sample
+    render("game/play_rock.html.erb")
+  end
 
+  def user_plays_paper
+    @user_move = "paper"
+    @computer_move = ["rock", "paper", "scissors"].sample
 
-  render ("game/play_rock.html.erb")
-end
+    if @user_move == @computer_move
+      @game_result = "tied"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @game_result = "won"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @game_result = "lost"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @game_result = "lost"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @game_result = "won"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @game_result = "lost"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @game_result = "won"
+    end
+
+    render("game/play_paper.html.erb")
+
+  end
+  def user_plays_scissors
+    @user_move = "scissors"
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @user_move == @computer_move
+      @game_result = "tied"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @game_result = "won"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @game_result = "lost"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @game_result = "lost"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @game_result = "won"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @game_result = "lost"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @game_result = "won"
+    end
+
+    render("game/play_scissors.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,5 +56,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   get("/rock", { :controller => "game", :action => "user_plays_rock" })
-
+    get("/paper", { :controller => "game", :action => "user_plays_paper" })
+  get("/scissors", { :controller => "game", :action => "user_plays_scissors" })
 end
